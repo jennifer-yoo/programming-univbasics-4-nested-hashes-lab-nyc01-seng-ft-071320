93e680c391174268a77670c5e57d83b51b61f72b
Jennifer Yoo <jennifer.yoo7@gmail.com>
2020-06-27 18:34:29 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -79,11 +79,11 @@ def adding_matz
         :languages => ["C"]
       }
     }
-		programmer_hash[:yukihiro_matsumoto] = {
-			:known_for => "Ruby",
-			:languages => ["LISP", "C"]
-		}
-		programmer_hash
+				programmer_hash[:yukihiro_matsumoto] = {
+					:known_for => "Ruby",
+					:languages => ["LISP", "C"]
+				}
+					programmer_hash
 end
 
 def changing_alan
@@ -105,7 +105,8 @@ def changing_alan
         :languages => ["C"]
       }
     }
-
+programmer_hash[:alan_kay][:known_for] = alans_new_info
+programmer_hash
 end
 
 def adding_to_dennis
